ed645e838d1991c1745a9d7826119b107e485359
张海生 <zhaisheng@rolaner.com>
2017-06-05 14:48:52 +0800

    最新

--- test/index.php
+++ test/index.php
@@ -9,8 +9,8 @@ require './../vendor/autoload.php';
 $dir = new \Dir\Dir();
 
 $files = $dir->path("D:/zimu/srts/unsaved")->gets();
-//var_dump($files);
-//
-//foreach ($files as $file){
-//    var_dump(file_get_contents($file));
-//}
+var_dump($files);
+
+foreach ($files as $file){
+    var_dump(file_get_contents($file));
+}
